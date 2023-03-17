dd3d0b2e3c972f7ec69ff68241e532cb806bf17f
Ahmad-PC <a_sharifianzade@arnika.ai>
2023-03-17 18:52:30 +0330

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+const suggestBtn = document.getElementById("suggest-btn");
+const foodName = document.getElementById("food-name");
+const foodIngredients = document.getElementById("food-ingredients");
+const foodRecipe = document.getElementById("food-recipe");
+const foodDetails = document.getElementById("food-details");
+
+suggestBtn.addEventListener("click", suggestFood);
+
+function suggestFood() {
+
+  fetch("foods.json")
+    .then(response => response.json())
+    .then(foods => {
+      const randomIndex = Math.floor(Math.random() * foods.length);
+      const food = foods[randomIndex];
+
+      foodName.textContent = food.name;
+      foodIngredients.innerHTML = "";
+      food.ingredients.forEach(ingredient => {
+        const liElement = document.createElement("li");
+        liElement.textContent = ingredient;
+        foodIngredients.appendChild(liElement);
+      });
+      foodRecipe.textContent = food.recipe;
+
+      foodDetails.classList.remove("d-none");
+    });
+}
